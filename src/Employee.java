7691bc5404660dadd5fe45387f322aeda7d11f16
Hanieh Sadri <haniehsadri77@gmail.com>
2021-03-29 05:48:24 +0430

    first code

---
+++ src/Employee.java
@@ -0,0 +1,14 @@
+public class Employee {
+    private String name;
+    private String lastname;
+    private int employeeId;
+    private String responsibility;
+
+
+    public Employee(String name, String lastname, int employeeId, String responsibility) {
+        this.name = name;
+        this.lastname = lastname;
+        this.employeeId = employeeId;
+        this.responsibility = responsibility;
+    }
+}
