bdf2ceaab67765c7b5b09db9e14d483c4fff2d36
Hanieh Sadri <haniehsadri77@gmail.com>
2021-04-09 11:06:18 +0430

    new functions added in Office class

---
+++ src/Manager.java
@@ -0,0 +1,7 @@
+public class Manager extends Employee {
+    public Manager(String name, String lastname, int personalId, String responsibility,
+                   String nationalId, String password, String username,
+                   String employmentType, String fathersname, String education) {
+        super(name, lastname, personalId, responsibility, nationalId, password, username, employmentType, fathersname, education);
+    }
+}
--- src/Office.java
+++ src/Office.java
@@ -1,4 +1,32 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Office {
+    public static ArrayList<Employee> employees=new ArrayList<Employee>();
+
+    public static void main(String[] args) {
+
+    }
+
+    public  void Signin(){
+        Scanner input=new Scanner(System.in);
+        System.out.println("please enter your username:");
+        String username=input.next();
+        System.out.println("please enter your password :");
+        String password=input.next();
+       int currentemployee= findcurrentemployee(employees,username,password);
+        System.out.println(employees.get(currentemployee));
+
+    }
+    public int findcurrentemployee(ArrayList<Employee> employees, String username, String password) {
+        for (int i = 0; i < employees.size(); i++) {
+            if (employees.get(i).getUsername().equals(username) && employees.get(i).getPassword().equals(password)) {
+                return i;
+            }
+            System.out.println("username or password is not correct ");
+        }
+        return -1;
+    }
 
 
 }
